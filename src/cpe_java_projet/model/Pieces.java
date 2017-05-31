9feff05ed88a47dd0393428b740d668d53a8ecf8
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-05-31 09:15:46 +0200

    Adds methods to Pièces interface

--- src/cpe_java_projet/model/Pieces.java
+++ src/cpe_java_projet/model/Pieces.java
@@ -10,5 +10,17 @@ package cpe_java_projet.model;
  * @author nathanael
  */
 public interface Pieces {
+    public boolean caputre();
     
+    public Couleur getCouleur();
+    
+    public String getName();
+    
+    public int getX();
+    
+    public int getY();
+    
+    public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible);
+    
+    public boolean move(int xFinal, int yFinal);
 }
