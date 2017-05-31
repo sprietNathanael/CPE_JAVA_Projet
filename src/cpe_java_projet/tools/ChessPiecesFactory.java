d86bd272395f81d369c2919ab64ad9b6e527a7bd
Ilias Redissi <ilias.redissi@gmail.com>
2017-05-31 13:58:05 +0200

    Fix indentation

--- src/cpe_java_projet/tools/ChessPiecesFactory.java
+++ src/cpe_java_projet/tools/ChessPiecesFactory.java
@@ -11,51 +11,51 @@ import cpe_java_projet.model.Pieces;
 /**
  * @author francoise.perrin
  * Inspiration Jacques SARAYDARYAN, Adrien GUENARD
- * 
+ *
  * Classe qui fabrique une liste de pieces de jeu d'echec
  * de la couleur pass�e en param�tre
  *
  */
 public class ChessPiecesFactory {
 
-	/**
-	 * private pour ne pas instancier d'objets
-	 */
-	private ChessPiecesFactory() {
+    /**
+     * private pour ne pas instancier d'objets
+     */
+    private ChessPiecesFactory() {
 
-	}
+    }
 
-	/**
-	 * @param pieceCouleur
-	 * @return liste de pi�ces de jeu d'�chec
-	 */
-	public static List<Pieces> newPieces(Couleur pieceCouleur){
+    /**
+     * @param pieceCouleur
+     * @return liste de pi�ces de jeu d'�chec
+     */
+    public static List<Pieces> newPieces(Couleur pieceCouleur){
 
-		List<Pieces> pieces = null;
-		pieces = new LinkedList<Pieces>();
-		String initCouleur = (Couleur.BLANC == pieceCouleur ? "B_" : "N_" );
+        List<Pieces> pieces = null;
+        pieces = new LinkedList<Pieces>();
+        String initCouleur = (Couleur.BLANC == pieceCouleur ? "B_" : "N_" );
 
-		if (pieceCouleur != null){
-			for (int i = 0; i < ChessPiecePos.values().length; i++) {
+        if (pieceCouleur != null){
+            for (int i = 0; i < ChessPiecePos.values().length; i++) {
 
-				if (pieceCouleur.equals(ChessPiecePos.values()[i].couleur)) {
-					for (int j = 0; j < (ChessPiecePos.values()[i].coords).length; j++) {
-						String className = "cpe_java_projet.model." + ChessPiecePos.values()[i].nom;	// attention au chemin
-						Coord pieceCoord = ChessPiecePos.values()[i].coords[j];
-						pieces.add((Pieces) Introspection.newInstance (className,
-								new Object[] {pieceCouleur, pieceCoord}));
-					}
-				}
-			}
-		}
-		return pieces;
-	}
+                if (pieceCouleur.equals(ChessPiecePos.values()[i].couleur)) {
+                    for (int j = 0; j < (ChessPiecePos.values()[i].coords).length; j++) {
+                        String className = "cpe_java_projet.model." + ChessPiecePos.values()[i].nom;	// attention au chemin
+                        Coord pieceCoord = ChessPiecePos.values()[i].coords[j];
+                        pieces.add((Pieces) Introspection.newInstance (className,
+                                new Object[] {pieceCouleur, pieceCoord}));
+                    }
+                }
+            }
+        }
+        return pieces;
+    }
 
-	/**
-	 * Tests unitaires
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println(ChessPiecesFactory.newPieces(Couleur.BLANC));
-	}
+    /**
+     * Tests unitaires
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(ChessPiecesFactory.newPieces(Couleur.BLANC));
+    }
 }
