93316907b1afb7fe7d9ed30a9936d795cf22d9b7
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-05-31 11:35:48 +0200

    Implements Reine isMoveOk() and Reine class tests

--- src/cpe_java_projet/model/Reine.java
+++ src/cpe_java_projet/model/Reine.java
@@ -10,5 +10,24 @@ package cpe_java_projet.model;
  * @author nathanael
  */
 public class Reine extends AbstractPiece{
+
+    public Reine(Couleur couleur, Coord coord) {
+        super(couleur, coord);
+    }
+
+    @Override
+    public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible) {
+        int deltaX = xFinal - this.coord.x;
+        int deltaY = yFinal - this.coord.y;
+        if(Math.abs(deltaX) > 0 && (deltaY == 0 || Math.abs(deltaY) == Math.abs(deltaX)))
+        {
+            return true;
+        }
+        else if(Math.abs(deltaY) > 0 && (deltaX == 0 || Math.abs(deltaY) == Math.abs(deltaX)))
+        {
+            return true;
+        }
+        return false;
+    }
     
 }
