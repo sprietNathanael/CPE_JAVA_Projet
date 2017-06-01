5045f53fc5f0a7aa8de0a58a4a30c882c144c5ea
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-06-01 10:12:54 +0200

    Makes the pieces draggable without any constrain

--- src/cpe_java_projet/vue/ChessGameGUI.java
+++ src/cpe_java_projet/vue/ChessGameGUI.java
@@ -11,8 +11,11 @@ import cpe_java_projet.model.PieceIHM;
 import cpe_java_projet.tools.ChessImageProvider;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -32,6 +35,10 @@ public class ChessGameGUI extends JFrame implements java.util.Observer, MouseLis
     
     JPanel chessBoard;
     JLayeredPane layeredPane;
+    JLabel chessPiece;
+    
+    int xAdjustment;
+    int yAdjustment;
 
     public ChessGameGUI(String jeu_déchec, ChessGameControlers chessGameControler, Dimension dim)
     {
@@ -40,8 +47,8 @@ public class ChessGameGUI extends JFrame implements java.util.Observer, MouseLis
         layeredPane = new JLayeredPane();
         getContentPane().add(layeredPane);
         layeredPane.setPreferredSize(dim);
-        //layeredPane.addMouseListener(this);
-        //layeredPane.addMouseMotionListener(this);
+        layeredPane.addMouseListener(this);
+        layeredPane.addMouseMotionListener(this);
         
         //Add a chess board to the Layered Pane 
  
@@ -87,37 +94,61 @@ public class ChessGameGUI extends JFrame implements java.util.Observer, MouseLis
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        chessPiece = null;
+        Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+ 
+        if (c instanceof JPanel) 
+            return;
+ 
+        Point parentLocation = c.getParent().getLocation();
+        xAdjustment = parentLocation.x - e.getX();
+        yAdjustment = parentLocation.y - e.getY();
+        chessPiece = (JLabel)c;
+        chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
+        chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+        layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(chessPiece == null) return;
+ 
+        chessPiece.setVisible(false);
+        Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+ 
+        if (c instanceof JLabel){
+            Container parent = c.getParent();
+            parent.remove(0);
+            parent.add( chessPiece );
+        }
+        else {
+            Container parent = (Container)c;
+            parent.add( chessPiece );
+        }
+ 
+        chessPiece.setVisible(true);
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void mouseDragged(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void mouseDragged(MouseEvent me) {
+        if (chessPiece == null) return;
+         chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
